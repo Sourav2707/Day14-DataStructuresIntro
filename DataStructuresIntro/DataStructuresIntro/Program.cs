df35ee97b829579857efed4723d0e77f9a32bbf1
Sourav2707 <smartsourav33@gmail.com>
2022-07-23 16:45:19 +0530

    creating linkedlist

--- DataStructuresIntro/DataStructuresIntro/Program.cs
+++ DataStructuresIntro/DataStructuresIntro/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Welcome to data structures program using generics");
+﻿using DataStructuresIntro;
+
+Console.WriteLine("Welcome to data structures program using generics");
+LinkedList list = new LinkedList();
+list.Add(56);
+list.Add(30);
+list.Add(70);
+list.Display();
